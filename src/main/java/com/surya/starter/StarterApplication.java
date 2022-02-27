651ab61ce4a137983e29b3587052e1bcc4136392
Surya Kandula <skandula@atlassian.com>
2022-02-27 21:57:25 +0530

    Add Lombok, RestTemplate, application.properties

--- src/main/java/com/surya/starter/StarterApplication.java
+++ src/main/java/com/surya/starter/StarterApplication.java
@@ -2,11 +2,18 @@ package com.surya.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class StarterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StarterApplication.class);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
